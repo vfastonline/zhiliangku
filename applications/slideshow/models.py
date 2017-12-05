37381b74594273a258e1e11ebeab8bee4658750f
xuhuiliang <15011544971@163.com>
2017-12-05 20:05:30 +0800

    轮播顺序可空

--- applications/slideshow/models.py
+++ applications/slideshow/models.py
@@ -1,7 +1,6 @@
 #!encoding:utf-8
 from __future__ import unicode_literals
 
-from django.core.validators import MinValueValidator
 from django.db import models
 
 from lib.storage import ImageStorage
@@ -16,7 +15,7 @@ class Carousel(models.Model):
     name = models.CharField('轮播名称', max_length=50)
     pathwel = models.ImageField('轮播图片', upload_to='carousel/%Y%m%d', storage=ImageStorage())
     category = models.CharField('类别', max_length=1, choices=CATEGORY, default="1")
-    sequence = models.PositiveIntegerField('播放顺序', default=1, validators=[MinValueValidator(1)])
+    sequence = models.PositiveIntegerField('播放顺序', blank=True)
 
     def __unicode__(self):
         return self.name
