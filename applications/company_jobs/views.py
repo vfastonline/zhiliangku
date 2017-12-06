fbf7bda6eac5e7deec0ad1bf87da6130b8e18737
sky-du <duminchao>
2017-12-06 10:05:51 +0800

    modify company_jobs  bug

--- applications/company_jobs/views.py
+++ applications/company_jobs/views.py
@@ -5,6 +5,7 @@ from applications.company_jobs.models import Company_jobs
 import traceback
 import json
 import os
+import logging
 from django.conf import settings
 
 
@@ -12,13 +13,16 @@ from django.conf import settings
 def company_add(request):
     try:
         if request.method == "POST":
+            logging.getLogger().info('test')
             data = request.POST
             token = data.get('token')
             if token != 'xuyang':
+                logging.getLogger().error('company_add invalid token')
                 return HttpResponse(json.dumps({'code': 1, 'msg': 'error token'}, ensure_ascii=False))
             tmp = dict([(k, v) for k, v in data.iteritems()])
             logoimg = request.FILES['logoimg']
             if logoimg.size > 102400:
+                logging.getLogger().error('company_add picture size over 100kb')
                 return HttpResponse(json.dumps({'code': 1, 'msg': 'picture size limit 100kb'}, ensure_ascii=False))
             logoimg_name = logoimg.name
             destination = os.path.join(settings.MEDIA_ROOT, 'companylogo')
@@ -31,9 +35,8 @@ def company_add(request):
             tmp['logo'] = '/media/companylogo/%s' % logoimg_name
             tmp.pop('token')
             Company_jobs.objects.create(**tmp)
-        else:
-            return HttpResponse('get')
+            logging.getLogger().info('company_add create company_job successfully')
+            return HttpResponse(json.dumps({'code': 0, 'msg': 'request successfully'}))
     except:
+        logging.getLogger().error(traceback.format_exc())
         return HttpResponse(traceback.format_exc())
-    finally:
-        return HttpResponse(json.dumps({'code': 0, 'msg': 'request successfully'}))
