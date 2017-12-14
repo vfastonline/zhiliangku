86d934ebdeaf5606807b0968e14962f3f491ceb3
xuhuiliang <15011544971@163.com>
2017-12-14 15:25:07 +0800

    增加验证码表，验证码后台验证

--- zhiliangku/settings.py
+++ zhiliangku/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates', 'static')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
@@ -210,7 +210,7 @@ SUIT_CONFIG = {
         # {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
 
         # 用户管理
-        {'app': 'custom_user', 'models': ('CustomUser', 'CustomUserAuths', 'CustomUserPath')},
+        {'app': 'custom_user', 'models': ('CustomUser', 'CustomUserAuths', 'CustomUserPath', "VerifyCode")},
 
         # 职业路径
         {'app': 'tracks_learning', 'models': ('Path', 'PathStage', 'CourseCategory')},
