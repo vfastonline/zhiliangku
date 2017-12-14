2196b2a80b72639004b3f40f52d23892fd056ab8
xie Jiabing <1131312177@qq.com>
2017-12-14 15:01:40 +0800

    2017-12-14 编译之后的代码对接

---
+++ zhiliangku-front/src/assets/js/const.js
@@ -0,0 +1,6 @@
+module.exports = (function () {
+    var obj = {
+        httpUrl:'http://47.93.229.122/'
+    };
+    return obj
+  })();
---
+++ zhiliangku-front/src/components/courseInfo/pathInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mainwidth incenter pathInfo">
+    <path-info-left :mainData="allData"></path-info-left>
+    <path-info-right :mainData="allData"></path-info-right>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      allData:{}
+    }
+  },
+  created(){
+    this.$ajax.post('tracks/path/detail',{path_id:1}).then(res=>{
+       this.$fn.addObjString(this.$myConst.httpUrl,res.data.data,['path_img'])
+       this.allData=res.data.data;
+      console.log(res)
+    })
+  }
+}
+</script>
+<style scoped>
+.pathInfo{
+    padding-top:60px;
+}
+</style>
+
+
+
