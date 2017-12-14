c491caad69d9dabeccd0713c869f1adf5d36151d
xuhuiliang <15011544971@163.com>
2017-12-14 16:52:29 +0800

    修改文件路径

--- zhiliangku/views.py
+++ zhiliangku/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def index(request):
     logging.getLogger().info('hello')
-    return render(request, 'front/index.html')
+    return render(request, 'index.html')
