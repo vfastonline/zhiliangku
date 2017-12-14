cbaed08f90ab66fde1582b1c295ce685ba38fa05
sky-du <duminchao@qq.com>
2017-12-14 16:48:30 +0800

    setting.py modify

--- zhiliangku/settings.py
+++ zhiliangku/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'zhiliangku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['/usr/local/openresty/nginx/html/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -162,7 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'templates', 'static')
+STATIC_ROOT = '/usr/local/openresty/nginx/html/templates/static'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
