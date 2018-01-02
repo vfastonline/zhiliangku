02e8a3b486523cc434691cd37de5c9ae637d6b8c
xuhuiliang <15011544971@163.com>
2018-01-02 12:24:54 +0800

    优化用户收藏课程机制

--- applications/tracks_learning/collect_course.py
+++ applications/tracks_learning/collect_course.py
@@ -9,12 +9,12 @@ from applications.tracks_learning.models import *
 from lib.permissionMixin import class_view_decorator, user_login_required
 
 
-@class_view_decorator(user_login_required)
+# @class_view_decorator(user_login_required)
 class CollectCourse(View):
     """收藏/取消收藏这个课程"""
 
     def post(self, request, *args, **kwargs):
-        result_dict = {"err": 0, "msg": "成功这个这个课程"}
+        result_dict = {"err": 0, "msg": "成功收藏这个课程"}
         try:
             param_dict = json.loads(request.body)
             course_id = int(param_dict.get('course_id', 0))  # 必填，课程ID
@@ -25,16 +25,23 @@ class CollectCourse(View):
                 courses = Course.objects.filter(id=course_id)
                 customusers = CustomUser.objects.filter(id=custom_user_id)
                 if courses.exists() and customusers.exists():
+                    customusercourses = CustomUserCourse.objects.filter(custom_user=customusers.first())
                     if is_collect:
-                        collect_obj = CustomUserCourse.objects.create(custom_user=customusers.first())
-                        collect_obj.course.add(courses.first())
-                        collect_obj.save()
+                        if not customusercourses.exists():
+                            collect_obj = CustomUserCourse.objects.create(custom_user=customusers.first())
+                            collect_obj.course.add(courses.first())
+                            collect_obj.save()
 
-                        if not collect_obj:
-                            result_dict["msg"] = "收藏失败"
+                            if not collect_obj:
+                                result_dict["msg"] = "收藏失败"
+                        else:
+                            add_obj = customusercourses.first()
+                            add_obj.course.add(courses.first())
+                            add_obj.save()
                     else:
-                        CustomUserCourse.objects.filter(custom_user=customusers.first(),
-                                                        course=courses.first()).delete()
+                        if customusercourses.exists():
+                            customusercourses.first().course.remove(courses.first())
+
                         result_dict["msg"] = "成功取消收藏"
                 else:
                     result_dict["err"] = 1
