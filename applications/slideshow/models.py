d1391a5fc6ba4fe17f3ebc4c32c6c04d00d91ada
xuhuiliang <15011544971@163.com>
2018-01-16 15:57:21 +0800

    轮播图添加关联路径信息

--- applications/slideshow/models.py
+++ applications/slideshow/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 from lib.storage import ImageStorage
+from applications.tracks_learning.models import Path
 
 
 class Carousel(models.Model):
@@ -17,9 +18,12 @@ class Carousel(models.Model):
     name = models.CharField('轮播名称', max_length=50)
     pathwel = models.ImageField('轮播图片', upload_to='carousel/%Y%m%d', storage=ImageStorage())
     category = models.CharField('类别', max_length=1, choices=CATEGORY, default="1")
+    path = models.ForeignKey(Path, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='职业路径',
+                             help_text="选填")
     sequence = models.PositiveIntegerField('播放顺序', blank=True, default=1, validators=[MinValueValidator(1)],
                                            help_text="从1开始，默认顺序为1")
     desc = models.TextField('轮播简介', max_length=1000, blank=True, null=True, default='')
+
 
     def __unicode__(self):
         return self.name
--- applications/slideshow/views.py
+++ applications/slideshow/views.py
@@ -26,6 +26,7 @@ class SlideList(View):
                         "pathwel": one.pathwel.url,
                         "category_name": one.get_category_display(),
                         "category": one.category,
+                        "path_id": one.path.id if one.path else "",
                         "sequence": one.sequence if one.sequence else 0,
                         "desc": one.desc
                     }
