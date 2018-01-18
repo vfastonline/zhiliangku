faa92425710ec9e7506d19ccf21c8e16d67d12dc
[xiejiabing1024] <xie1248xie@163.com>
2018-01-18 15:01:16 +0800

    afs

---
+++ zhiliangku-front/src/components/videoDetail/sectionCommen.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="sc-container rise">
+    <div class=" sc-question-container">
+      <div class="floatl font20pl3a3c50 sc-tag">Q:</div>
+      <div class="sc-question-title font14pl3a3c50" v-html="mainData.question"></div>
+    </div>
+    <div class="sc-question-content font14pl3a3c50" v-html="mainData.answer"></div>
+  </div>
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" >
+  .sc-container {
+    padding: 31px 40px 24px 40px;
+    background: white;
+    margin-bottom:24px;
+  }
+
+  .sc-tag {
+    opacity: 0.5;
+  }
+
+  .sc-question-container {
+    line-height: 32px;
+  }
+
+  .sc-question-title {
+    margin-left: 33px;
+    pre {
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+  }
+
+  .sc-question-content {
+    margin-left: 32px;
+    opacity: 0.6;
+    pre {
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+  }
+
+</style>
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    props: {
+      mainData: Object
+    },
+    created() {
+
+    }
+  }
+
+</script>
