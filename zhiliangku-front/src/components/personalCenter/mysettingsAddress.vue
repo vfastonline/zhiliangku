9255352c2f4019bd372eb6922441a9f2c52e6d17
[xiejiabing1024] <xie1248xie@163.com>
2018-01-18 14:39:42 +0800

    afs

---
+++ zhiliangku-front/src/components/personalCenter/mysettingsAddress.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="settings-container incenter">
+    <div class="setting-item relative">
+      <div class="tag font14pl3a3c50">收货地址：</div>
+      <div class="set-content">
+        <el-input v-model="value0"></el-input>
+      </div>
+    </div>
+    <div class="setting-item relative">
+      <div class="tag font14pl3a3c50">收货人：</div>
+      <div class="set-content">
+        <el-input v-model="value1"></el-input>
+      </div>
+    </div>
+    <div class="setting-item relative">
+      <div class="tag font14pl3a3c50">联系电话：</div>
+      <div class="set-content">
+        <el-input v-model="value2"></el-input>
+      </div>
+    </div>
+    <rcb   @submit="submit(orgnizeData())" :layer="['submit']"></rcb>
+  </div>
+</template>
+<script>
+import rcb from './resumeContentButton'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        value0:'',
+        value1:'',
+        value2:'',
+      }
+    },
+    methods:{
+      orgnizeData(){
+        var obj={
+          custom_user_id:localStorage.uid,
+          receiver:this.value0,
+          address:this.value1,
+          contact_number:this.value2
+        }
+        return obj
+      },
+      submit(data){
+        if(!data)return;
+        this.$post('/personal_center/personal_settings/user/address',data).then(res=>{
+          if(!res.data.err){
+            this.$notify({
+              type: 'success',
+              message: '已为您设置成功',
+              offset: 100,
+              duration: 3000,
+              position: 'bottom-right'
+            });
+          }
+        })
+      },
+    },
+    created(){
+      this.$get('/personal_center/personal_settings/user/address?custom_user_id='+localStorage.uid).then(res=>{
+        this.initForm(this, res.data.data, ['receiver', 'address', 'contact_number'], [
+        'value0','value1','value2'
+      ])
+      })
+    },
+    components:{
+        rcb:rcb
+    }
+  }
+</script>
+<style lang='scss' scoped>
+@import '../../style/scss/components/mysettings.scss';
+</style>
+
---
+++ zhiliangku-front/src/components/videoDetail/video.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class=" incenter ">
+    <div id="e8888b74d1229efec6b4712e17cb6b7a_e">
+    </div>
+  </div>
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .videoModule {
+    background: red;
+    width: 1152px;
+  }
+
+</style>
+<script>
+  import Bus from '../../assets/js/bus'
+  export default {
+    data() {
+      return {
+        vid: ''
+      }
+    },
+    props: {
+
+    },
+    methods: {
+      video(vid) {
+        var obj = {
+          wrap: '#e8888b74d1229efec6b4712e17cb6b7a_e',
+          width: '100%',
+          height: 585,
+          vid: vid,
+        }
+        var player = polyvPlayer(obj);
+      },
+      liveVideo(id) {
+        var player = polyvObject('#e8888b74d1229efec6b4712e17cb6b7a_e').livePlayer({
+          width: '100%',
+          height: 585,
+          'uid': 'a582a3b650',
+          'vid': id
+        });
+      }
+    },
+    created() {
+      this.$on('vid', function (vid) {
+        this.video(vid)
+      })
+      this.$on('liveid', function (id) {
+        this.liveVideo(id)
+      })
+      Bus.$on('vid', (vid) => {
+        debugger
+        this.vid = vid;
+        this.video(vid)
+      })
+    },
+    mounted() {
+
+    }
+
+  }
+
+</script>
