4e46a1c2d9e2f5a89b767e4b6ec80f48255ec1ea
xuhuiliang <15011544971@163.com>
2018-01-18 16:44:08 +0800

    课程下视频添加是否学习信息

--- applications/tracks_learning/video_list.py
+++ applications/tracks_learning/video_list.py
@@ -13,6 +13,7 @@ from applications.tracks_learning.models import *
 from conf.conf_core import *
 from lib.permissionMixin import class_view_decorator, user_login_required
 from lib.util import str_to_int
+from applications.record.models import WatchRecord
 
 
 class UploadVideoPolyvParam(View):
@@ -41,6 +42,7 @@ class VideoList(View):
         try:
             # 获取查询参数
             section_id = str_to_int(request.GET.get('section_id', 0))
+            custom_user_id = str_to_int(request.GET.get('custom_user_id', 0))  # 用户ID
 
             data_list = list()
             if section_id:
@@ -53,8 +55,10 @@ class VideoList(View):
                     video_dict["name"] = one_video.name
                     video_dict["sequence"] = one_video.sequence
                     video_dict["desc"] = one_video.desc
-                    video_dict["is_learned"] = 1
+                    video_dict["is_learned"] = 0
                     video_dict["duration"] = one_video.duration
+
+                    # 补全直播信息
                     if one_video.live:
                         video_dict["live_id"] = one_video.live.id
                         video_dict["live_channelId"] = one_video.live.channelId
@@ -64,6 +68,12 @@ class VideoList(View):
                             if one_video.live_start_time else ""
                         video_dict["live_end_time"] = one_video.live_end_time.strftime("%H:%M") \
                             if one_video.live_end_time else ""
+                    # 补全是否学习状态
+                    if one_video.type in ["1", "2"]:  # 点播、直播回放
+                        if WatchRecord.objects.filter(video=one_video, user_id=custom_user_id).exists():
+                            video_dict["is_learned"] = 1
+                    if one_video.type == "4":  # 习题
+                        pass
 
                     data_list.append(video_dict)
             result_dict["data"] = data_list
