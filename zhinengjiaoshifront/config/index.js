c93e4fb5ce08ab70f4ae1c4b51ca6b7b9cfe38c1
xiejiabing <751632563@qq.com>
2018-05-11 12:37:22 +0800

    new

--- zhinengjiaoshifront/config/index.js
+++ zhinengjiaoshifront/config/index.js
@@ -52,10 +52,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../zhiliangku-front/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../zhiliangku-front'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
