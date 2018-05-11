63dc2666053faf7a6d6f46428ad1c72d257cf194
xiejiabing <751632563@qq.com>
2018-05-11 12:50:57 +0800

    new

--- zhiliangku-front/src/router/index.js
+++ zhiliangku-front/src/router/index.js
@@ -1,11 +1,15 @@
-// vue路由配置文件
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
 
-import Home from '../components/home/home.vue'
 export default new Router({
   routes: [
-    {path: '/',name: 'home',components:{'a':Home}}
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
   ]
 })
