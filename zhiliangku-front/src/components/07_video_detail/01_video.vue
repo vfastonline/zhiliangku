961f0bdf7a6c860ea9ed9351173c7c1a8712bc42
xiejiabing <751632563@qq.com>
2018-05-14 18:42:50 +0800

    add front build

---
+++ zhiliangku-front/src/components/07_video_detail/01_video.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mw hc">
+    <video-player id="my-video" :options="playerOptions"></video-player>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import VueVideo from 'vue-video-player'
+  import 'video.js/dist/video-js.css'
+
+  Vue.use(VueVideo)
+  export default {
+    name: "my_video",
+    data() {
+      return {
+        playerOptions: {
+          // video js options
+          muted: true,
+          language: 'en',
+          playbackRates: [1.0, 1.25, 1.5, 1.75, 2.0],
+          sources: [{
+            type: "video/mp4",
+            src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+          }],
+          fluid: true,
+          poster: ""// 海豹图片地址
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- zhiliangku-front/src/pages/04_video_detail/app.vue
+++ zhiliangku-front/src/pages/04_video_detail/app.vue
@@ -2,6 +2,7 @@
   <div>
     <div id="main">
       <MyHeader></MyHeader>
+      <MyVideo></MyVideo>
     </div>
     <F></F>
   </div>
@@ -13,14 +14,18 @@
 <script>
   import MyHeader from '../../components/01_header_footer/01_header'
   import F from '../../components/01_header_footer/03_footer'
+  import MyVideo from '../../components/07_video_detail/01_video'
 
   export default {
     data() {
       return {}
     },
+    methods: {
+    },
     components: {
       MyHeader: MyHeader,
-      F: F
+      F: F,
+      MyVideo:MyVideo
     }
   }
 </script>
