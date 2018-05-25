9de84b3c530a7d2ec4527290c12374e8312dc612
shijiangzhou <shi_jianghzou@sina.com>
2018-05-25 19:34:45 +0800

    assess-result

---
+++ zhiliangku-front/src/components/22_assess_result/03_message.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="message_box mw hc">
+    <div class="message_top ftc font1_28_f">
+      <p>错误提示</p>
+    </div>
+    <div class="message_content font1_22_9">
+      <p>错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示</p>
+      <p>错误提示</p>
+      <p>错误提示 错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示错误提示</p>
+
+    </div>
+  </div>
+</template>
+<style>
+  .message_box {
+    height: 330px;
+    margin-top: 40px;
+    box-shadow: 0 0 18px rgba(0, 0, 0, 0.15);
+  }
+
+  .message_top {
+    margin-top: 40px;
+    height: 60px;
+    line-height: 60px;
+    background-color: #324e5c;
+  }
+
+  .message_content p {
+    line-height: 30px;
+    padding: 20px 60px 0 60px;
+  }
+</style>
+<script>
+
+</script>
--- zhiliangku-front/src/pages/22_assess_result/app.vue
+++ zhiliangku-front/src/pages/22_assess_result/app.vue
@@ -2,25 +2,74 @@
   <div>
     <div id="main">
       <MyHeader></MyHeader>
+      <div class="main_box mw hc">
+        <Success :status=a></Success>
+      </div>
+      <Message></Message>
+      <div class="bottom_button mw hc ftc">
+        <Button class="hc ftc">查看详情</Button>
+        <Button class="hc ftc">重新答题</Button>
+        <Button class="hc ftc">重新学习</Button>
+      </div>
+      <ImageBlock :src="$myConst.httpUrl+'/media/image/static/project_list_02_bottom.png'"></ImageBlock>
     </div>
     <F></F>
   </div>
 </template>
 <style scoped lang='scss'>
+  .main_box {
+    /*min-height:70vh;*/
+  }
 
+  .bottom_button {
+    margin-top: 30px;
+  }
 </style>
 
 <script>
   import MyHeader from '../../components/01_header_footer/01_header'
   import F from '../../components/01_header_footer/03_footer'
+  import Success from '../../components/22_assess_result/02_success'
+  import Button from '../../components/00_common/04_blue_button'
+  import ImageBlock from '../../components/00_common/08_image_block'
+  import Message from '../../components/22_assess_result/03_message'
+
 
   export default {
     data() {
-      return {}
+      return {
+        status_arr: ['success', 'failure'],
+        a: 0,//临时数据
+        main_data: {}
+      }
     },
     components: {
       MyHeader: MyHeader,
-      F: F
+      F: F,
+      Success: Success,
+      Button: Button,
+      ImageBlock: ImageBlock,
+      Message: Message
+    },
+    methods: {
+      getResult() {
+        let video_id = this.$fn.funcUrl("video_id");
+        let obj = {
+          'video_id': video_id
+        }
+        this.$post("/assess/result/info", obj).then(res => {
+          // console.log("res.data")
+          //请求数据 接口报错了。
+          if (!res.err) {
+            this.main_data = res.data
+          }
+
+        })
+      }
+    },
+    created() {
+      this.getResult()
     }
+
   }
 </script>
