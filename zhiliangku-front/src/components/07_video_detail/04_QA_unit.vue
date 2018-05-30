0af227bd1fde97fd135fa510a0ebed7f524d842e
shijiangzhou <shi_jianghzou@sina.com>
2018-05-30 11:00:38 +0800

    faq-detail style

--- zhiliangku-front/src/components/07_video_detail/04_QA_unit.vue
+++ zhiliangku-front/src/components/07_video_detail/04_QA_unit.vue
@@ -1,7 +1,9 @@
 <template>
   <div @click="handleClick" class="mw  qc-container">
     <div class="clearfix">
-      <!--<img class="fl qc-user-icon" :src="mainData.custom_user_avatar" alt="">-->
+      <div class="icon_box ftc fl vm ">
+      <img class="qc-user-icon" :src="mainData.custom_user_avatar" alt="">        
+      </div>
       <div class=" qc-text-container">
         <div class="mt8" >
           <span class="font1_14_9 qctc-tag">{{mainData.custom_user_nickname}}</span>
@@ -10,11 +12,11 @@
         <div class="font1_20_3 clearfix mt8">
           <div class="fl qctc-qustion-content">{{mainData.title}}</div>
           <div class="fr">
-            <span class="font1_16_3 anwser">{{mainData.faq_answer_count}}回答</span>
+            <span class="font1_16_3 clearfix anwser">{{mainData.faq_answer_count}}回答</span>
             <span class="font1_16_3">{{mainData.browse_amount}}次浏览</span>
           </div>
         </div>
-        <div>
+        <div class="clearfix">
           <!--<div class="font1_14_23b8ff">悬赏 {{mainData.reward}} 积分</div>-->
           <div class="font1_14_23b8ff">{{mainData.status_name}}</div>
         </div>
@@ -28,10 +30,13 @@
   }
   .qc-container {
     box-sizing: border-box;
-    padding: 24px 32px 38px 32px;
+    padding: 24px 32px 38px 0;
     background: #fcfcfc;
     margin-bottom: 20px;
     border-radius: 10px 
+  }
+  .icon_box {
+    width: 120px;
   }
   .mt8 {
     margin-bottom: 8px;
@@ -41,10 +46,11 @@
     width: 64px;
     height: 64px;
     border-radius: 50%;
+    padding-top:5px;
   }
 
   .qc-text-container {
-    margin-left: 80px;
+    margin-left: 120px;
   }
 
   .qctc-tag {
