882e9efe03973a287b0f452e9087f2d0677bbab9
xuhuiliang <15011544971@163.com>
2018-06-05 12:33:16 +0800

    增加注释

--- applications/personal_center/progress.py
+++ applications/personal_center/progress.py
@@ -23,12 +23,15 @@ class GetLearningProgressInfo(View):
 	def get(self, request, *args, **kwargs):
 		try:
 			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
+
+			# 对用户观看记录以项目信息分组查询
 			watchrecords = WatchRecord.objects.filter(user__id=custom_user_id).values("course__project").annotate(
 				Count('id'))
 			time_line_dict = dict()
 			for one in watchrecords:
 				project_id = one.get("course__project")
 
+				# 查询用户指定项目最近一次学习记录
 				filter_param = {
 					"user__id": custom_user_id,
 					"course__project__id": project_id,
@@ -41,12 +44,18 @@ class GetLearningProgressInfo(View):
 				detail["name"] = record.course.project.name
 				detail["pathwel"] = record.course.project.pathwel.url if record.course.project.pathwel else ""
 
-				courses = record.course.project.Courses.all()
-				courses_count = courses.count()
+				courses = record.course.project.Courses.all()  # 项目下所有课程
+				courses_count = courses.count()  # 课程数
 				courses_schedule = 0
 				for course in courses:
+					# 汇总用户每个课程完成百分比
 					summarize_param = [custom_user_id, course, list(courses)]
 					summarize_dict = project_summarize_course_progress(*summarize_param)
+					unlock = summarize_dict.get("unlock")
+
+					# 课程未解锁，跳出汇总
+					if not unlock:
+						break
 					courses_schedule += summarize_dict.get("schedule")
 					learn_video_id = summarize_dict.get("learn_video_id")
 					if learn_video_id:
@@ -55,8 +64,11 @@ class GetLearningProgressInfo(View):
 						detail["learn_video_type"] = summarize_dict.get("learn_video_type")
 						detail["learn_video_name"] = summarize_dict.get("learn_video_name")
 
+				# 项目完成百分比
 				detail["schedule"] = float(courses_schedule) / float(courses_count)
 				time_line_dict[create_time] = detail
+
+			# 对时间轴排序，观看时间倒序
 			data_list = list()
 			for i in sorted(time_line_dict.items(), key=lambda e: e[0], reverse=True):
 				data_list.append({i[0]: i[1]})
