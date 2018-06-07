cfffe92b3f067a55bd8aaef65cd8cf376644dbd0
xiejiabing <751632563@qq.com>
2018-06-07 17:21:27 +0800

    new

---
+++ zhiliangku-front/src/pages/12_notification/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="main">
+      <MyHeader></MyHeader>
+      <MessageCenter></MessageCenter>
+    </div>
+    <F></F>
+  </div>
+</template>
+<style scoped lang='scss'>
+
+</style>
+
+<script>
+  import MyHeader from '../../components/01_header_footer/01_header'
+  import F from '../../components/01_header_footer/03_footer'
+  import  MessageCenter from '../../components/14_notification/01_message_center'
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      MyHeader: MyHeader,
+      F: F,
+      MessageCenter:MessageCenter
+    }
+  }
+</script>
