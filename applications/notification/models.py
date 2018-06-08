9b6bacb57ff86736b9044a09b1a81e7d2c16aa45
xuhuiliang <15011544971@163.com>
2018-06-08 15:14:19 +0800

    只有当这个instance是新创建，而且have_read是默认的false才更新

--- applications/notification/models.py
+++ applications/notification/models.py
@@ -116,11 +116,7 @@ def increment_notifications_counter(sender, instance, created, **kwargs):
 	"""
 	try:
 		# 只有当这个instance是新创建，而且have_read是默认的false才更新
-		if created:
-			if instance.have_read:
-				NotificationController(instance.custom_user).update_unread_count(-1)
-			else:
-				NotificationController(instance.custom_user).update_unread_count(1)
+		if not (created and not instance.have_read):
 			return
 		# 调用 update_unread_count 方法来更新计数器 +1
 		NotificationController(instance.custom_user).update_unread_count(1)
