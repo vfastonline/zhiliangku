7da55e32d40ae7d8a629e25a9871870a38f2b35a
xuhuiliang <15011544971@163.com>
2018-06-08 17:38:58 +0800

    增加获取微信推广AccessToken

--- applications/wechat_promotion/views.py
+++ applications/wechat_promotion/views.py
@@ -1,4 +1,5 @@
 #!encoding:utf-8
+import requests
 from django.db.models import F
 from django.shortcuts import render
 from django.views.generic import View
@@ -62,3 +63,38 @@ class WechatThumbsUp(View):
 			result_dict["msg"] = traceback.format_exc()
 		finally:
 			return HttpResponse(json.dumps(result_dict, ensure_ascii=False))
+
+
+class GetAccessToken(View):
+	"""获取-AccessToken"""
+
+	def __init__(self):
+		super(GetAccessToken, self).__init__()
+		self.result_dict = {"err": 0, "msg": "success", "access_token": ""}
+		self.appid = 'wx7c9efe7b17c8aef2'
+		self.appsecret = '4f44d0ecc91e0dd9ef955885d6cfcb4f'
+		self.url = 'https://api.weixin.qq.com/cgi-bin/token'
+
+	def get(self, request, *args, **kwargs):
+
+		try:
+			params = {
+				'appid': self.appid,
+				'secret': self.appsecret,
+				'grant_type': 'client_credential'
+			}
+			res = requests.get(self.url, params=params, verify=False).json()
+			"""
+			{
+			u'access_token': u'10_EpnmZniSpsnf1ttZbGMS14O7ZQi7kZPcY52dJLHmcAlEQ_ndwUr4J2HX8s33JqUTjBKV-EDKeImlvYyXqGa9o0_expR7LSgzE5wLFCwzDDb_lHywV396HXhCug8Z4oTsb2OzzRFmuae4UvcENOVdADAXYF', 
+			u'expires_in': 7200
+			}
+			"""
+			self.result_dict["access_token"] = res.get("access_token", "")
+		except:
+			traceback.print_exc()
+			logging.getLogger().error(traceback.format_exc())
+			self.result_dict["err"] = 1
+			self.result_dict["msg"] = traceback.format_exc()
+		finally:
+			return HttpResponse(json.dumps(self.result_dict, ensure_ascii=False))
