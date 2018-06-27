0f2d937e6df00d0c0218ec55ea42af549facd8dd
xiejiabing <751632563@qq.com>
2018-06-27 18:41:26 +0800

    s

--- zhiliangku-front/src/pages/26_we_chat_world_cup/app.vue
+++ zhiliangku-front/src/pages/26_we_chat_world_cup/app.vue
@@ -108,7 +108,7 @@ export default {
       this.$get(url + '/wechat/get/signature?urls=' + encodeURIComponent(window.location.href.split('#')[0]), function (data) {
         var content = JSON.parse(data).data
         var config = {
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: content.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
           timestamp: content.timestamp, // 必填，生成签名的时间戳
           nonceStr: content.nonceStr, // 必填，生成签名的随机串
