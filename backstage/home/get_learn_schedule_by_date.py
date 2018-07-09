4013769082497a74d2714076530fb0b3ccd775ac
xuhuiliang <15011544971@163.com>
2018-07-09 10:36:56 +0800

    转化时间类型

--- backstage/home/get_learn_schedule_by_date.py
+++ backstage/home/get_learn_schedule_by_date.py
@@ -1,6 +1,5 @@
 #!encoding:utf-8
 import datetime
-from datetime import datetime
 
 from django.db.models import Avg
 from django.views.generic import View
@@ -35,7 +34,7 @@ class GetLearnTaskScheduleBydate(View):
 	def get(self, request, *args, **kwargs):
 		try:
 			today_date = get_day_of_day(0)
-			today_date = datetime(today_date.year, today_date.month, today_date.day)
+			today_date = datetime.datetime(today_date.year, today_date.month, today_date.day)
 			get_date = request.GET.get('get_date', "")  # 今日学习任务视频ID
 			if not get_date:
 				get_date = today_date
