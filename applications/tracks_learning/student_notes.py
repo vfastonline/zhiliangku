52bfbd46dc481b5d9f90a8c86f3442889e8314f1
p <pidatou233@outlook.com>
2018-08-01 18:27:07 +0800

    重构学生笔记接口

--- applications/tracks_learning/student_notes.py
+++ applications/tracks_learning/student_notes.py
@@ -155,7 +155,7 @@ class DeleteStudentNotes(View):
 			return HttpResponse(json.dumps(result_dict, ensure_ascii=False))
 
 
-# @class_view_decorator(user_login_required)
+@class_view_decorator(user_login_required)
 class StudentNotesUpdateView(APIView):
 	"""
 	学生笔记：删除，修改
@@ -168,10 +168,10 @@ class StudentNotesUpdateView(APIView):
 			"msg": "success"
 		}
 		try:
-			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
 			pk = kwargs.get('pk', 0)  # 学生笔记ID
-
-
+			deleted, _rows_count = StudentNotes.objects.filter(pk=pk).delete()
+			if not deleted:
+				result_dict["msg"] = u"未找到要删除的笔记信息"
 		except:
 			traceback.print_exc()
 			logging.getLogger().error(traceback.format_exc())
@@ -190,11 +190,30 @@ class StudentNotesUpdateView(APIView):
 		try:
 			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
 			pk = kwargs.get('pk', 0)  # 学生笔记ID
-			print pk
 			param_dict = json.loads(request.body)
-			aaa = param_dict.get("aaa", 0)
-			print aaa
-			result_dict["data"] = [1, 2, 3, 4, 6]
+			video_id = str_to_int(param_dict.get('video_id', 0))  # 视频id
+			title = param_dict.get('title', "")
+			notes = param_dict.get('notes', "")
+			filter_param = {
+				"pk": pk,
+				"video__id": video_id,
+				"custom_user__id": custom_user_id
+			}
+
+			studentnotes = StudentNotes.objects.filter(**filter_param)
+			if studentnotes.exists():
+				update_param = {
+					"title": title,
+					"notes": notes
+				}
+				rows = studentnotes.update(**update_param)
+				if not rows:
+					result_dict["err"] = 1
+					result_dict["msg"] = u"修改失败"
+			else:
+				result_dict["err"] = 1
+				result_dict["msg"] = u"无笔记信息"
+
 		except:
 			traceback.print_exc()
 			logging.getLogger().error(traceback.format_exc())
@@ -202,26 +221,63 @@ class StudentNotesUpdateView(APIView):
 			result_dict["msg"] = traceback.format_exc()
 		finally:
 			render = JSONRenderer().render(result_dict)
-			return Response(result_dict)
+			return Response(render)
 
 
-# @class_view_decorator(user_login_required)
+@class_view_decorator(user_login_required)
 class StudentNotesView(APIView):
-	"""
-	学生笔记：增加，查询
-	"""
+
 
 	# 查询
 	def get(self, request, *args, **kwargs):
 		result_dict = {
 			"err": 0,
-			"msg": "success"
+			"msg": "success",
+			"data": [],
+			"paginator": dict()
 		}
 		try:
+			video_id = str_to_int(request.GET.get('video_id', 0))  # 视频ID
 			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
-			aaa = request.GET.get("aaa", 0)
-			print aaa
-			result_dict["data"] = [1, 2, 3, 4, 5]
+			page = self.request.GET.get("page", 1)  # 页码
+			per_page = self.request.GET.get("per_page", 10)  # 每页显示条目数
+
+			filter_param = {
+				"video__id": video_id,
+				"custom_user__id": custom_user_id,
+			}
+			student_notes = StudentNotes.objects.filter(**filter_param)
+			# 提供分页数据
+			if not page: page = 1
+			if not per_page: page = 10
+			page_obj = Paginator(student_notes, per_page)
+			total_count = page_obj.count  # 记录总数
+			num_pages = page_obj.num_pages  # 总页数
+			page_range = list(page_obj.page_range)  # 页码列表
+			paginator_dict = {
+				"total_count": total_count,
+				"num_pages": num_pages,
+				"page_range": page_range,
+				"page": page,
+				"per_page": per_page
+			}
+
+			result_dict["paginator"] = paginator_dict
+			try:
+				student_notes = page_obj.page(page).object_list
+			except:
+				student_notes = list()
+
+			data_list = list()
+			for note in student_notes:
+				note_dict = dict()
+				note_dict["id"] = note.id
+				note_dict["title"] = note.title
+				note_dict["notes"] = note.notes
+				note_dict["custom_user"] = note.custom_user.nickname if note.custom_user else ""
+				note_dict["create_time"] = note.create_time.strftime("%Y-%m-%d %X")
+				data_list.append(note_dict)
+			result_dict["data"] = data_list
 		except:
 			traceback.print_exc()
 			logging.getLogger().error(traceback.format_exc())
@@ -235,14 +291,41 @@ class StudentNotesView(APIView):
 	def post(self, request, *args, **kwargs):
 		result_dict = {
 			"err": 0,
-			"msg": "success"
+			"msg": "success",
+			"data": {}
 		}
 		try:
-			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
 			param_dict = json.loads(request.body)
-			aaa = param_dict.get("aaa", 0)
-			print aaa
-			result_dict["data"] = [1, 2, 3, 4, 6]
+			video_id = str_to_int(param_dict.get('video_id', 0))  # 视频id
+			custom_user_id = str_to_int(kwargs.get('uid', 0))  # 用户ID
+			title = param_dict.get('title', "")
+			notes = param_dict.get('notes', "")
+
+			if title and notes:
+				custom_user = CustomUser.objects.filter(id=custom_user_id)
+				video = Video.objects.filter(id=video_id)
+				if video.exists() and custom_user.exists():
+					create_dict = {
+						"video": video.first(),
+						"custom_user": custom_user.first(),
+						"title": title,
+						"notes": notes,
+					}
+					student_notes = StudentNotes.objects.create(**create_dict)
+					if student_notes:
+						result_dict["data"] = {
+							"id": student_notes.id,
+							"notes": student_notes.notes,
+							"title": student_notes.title,
+							"custom_user": student_notes.custom_user.nickname if student_notes.custom_user else "",
+							"create_time": student_notes.create_time.strftime("%y-%m-%d %X"),
+						}
+				else:
+					result_dict["msg"] = u"缺少视频信息或用户信息"
+					result_dict["err"] = 1
+			else:
+				result_dict["msg"] = u"缺少笔记标题或内容"
+				result_dict["err"] = 1
 		except:
 			traceback.print_exc()
 			logging.getLogger().error(traceback.format_exc())
@@ -250,4 +333,4 @@ class StudentNotesView(APIView):
 			result_dict["msg"] = traceback.format_exc()
 		finally:
 			render = JSONRenderer().render(result_dict)
-			return Response(result_dict)
+			return Response(render)
