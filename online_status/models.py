f4ad154d43c013a6d1a62681a55d1d16011a5e2d
xuhuiliang <15011544971@163.com>
2018-08-10 17:22:43 +0800

    实现记录学生登录记录，

--- online_status/models.py
+++ online_status/models.py
@@ -20,23 +20,37 @@ class OnlineStatus(models.Model):
 	def __str__(self):
 		return '%s last login at UTC %s' % (self.user.nickname, self.last_login.strftime('%Y-%m-%d %H:%M'))
 
-	def get_last_active(self):
-		"""获取最后登录时间
+	@staticmethod
+	def get_last_active(user_id):
+		"""获取今天最后登录时间
+		:param user_id: 学生角色的用户ID
 		:return:
 		"""
-		cache_key = '%s_last_login' % self.user.id
+
+		cache_key = '%s_last_login' % user_id
 		# 如果缓存过期，从数据库获取last_login，并存到缓存
 		cache_last_login = redis_db.get(cache_key)
 		if not cache_last_login:
-			redis_db.setex(cache_key, self.last_login, settings.USER_ONLINE_TIMEOUT)
-			cache_last_login = self.last_login
+			today = datetime.datetime.today()
+			start_date = datetime.datetime(today.year, today.month, today.day, 0, 0, 0)
+			end_date = datetime.datetime(today.year, today.month, today.day, 23, 59, 59)
+			filter_dict = dict(user__id=user_id, last_login__range=(start_date, end_date))
+			online_status = OnlineStatus.objects.filter(**filter_dict)
+			if online_status.exists():
+				cache_last_login = online_status.first().last_login
+				redis_db.setex(cache_key, cache_last_login, settings.USER_ONLINE_TIMEOUT)
+
 		return cache_last_login
 
-	def is_online(self):
+	def is_online(self, user_id):
 		"""是否在线
-		:return: True/Flase
+		:param user_id: 学生角色的用户ID
+		:return: True/False
 		"""
 		now = timezone.now()
-		if self.get_last_active() < now - datetime.timedelta(seconds=settings.USER_ONLINE_TIMEOUT):
+		try:
+			if self.get_last_active(user_id) < now - datetime.timedelta(seconds=settings.USER_ONLINE_TIMEOUT):
+				return False
+			return True
+		except:
 			return False
-		return True
