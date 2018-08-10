925ee91256454decebde7c8dd55176a2dc2f26f3
caochao <caochao_mx@163.com>
2018-08-10 08:53:09 +0800

    xiugai

--- backstage/student_list/urls.py
+++ backstage/student_list/urls.py
@@ -5,9 +5,9 @@ from backstage.student_list.views import *
 
 urlpatterns = [
 	# 学员列表
-	url('^studentlist$', StudentList.as_view()),  # 学员列表信息
-	url('^studentinfo$', StudentInfo.as_view()),  # 学员基本信息+当前学习节点
-	url('^timeinfo$', TimeInfo.as_view()),  # 时间段信息
+	url('^student/list$', StudentList.as_view()),  # 学员列表信息
+	url('^student/info$', StudentInfo.as_view()),  # 学员基本信息+当前学习节点
+	url('^daterangeinfo', DateRangeInfo.as_view()),  # 时间段信息
 
 	#时间、视频分布信息
 	url('^learningtime$', LearningTime.as_view()),  # 时间分布信息
--- backstage/student_list/views.py
+++ backstage/student_list/views.py
@@ -1,4 +1,5 @@
 #!encoding:utf-8
+import datetime
 from django.db.models import Sum
 from rest_framework import status
 from rest_framework.views import APIView
@@ -86,7 +87,7 @@ class StudentList(APIView):
 			return JsonResponse(data=data, err=err, msg=msg, paginator=paginator)
 
 
-# @class_view_decorator(teacher_login_required)
+@class_view_decorator(teacher_login_required)
 class StudentInfo(APIView):
 	"""
 	学员基本信息+当前学习节点
@@ -99,60 +100,97 @@ class StudentInfo(APIView):
 
 		try:
 			name = self.request.GET.get("name", "")  # 姓名
-			param = dict(nickname__icontains=name)
-			param = get_kwargs(param)
-			user_list = CustomUser.objects.filter(**param)
+			if name:
+				param = dict(nickname__icontains=name)
+				param = get_kwargs(param)
+				user_list = CustomUser.objects.filter(**param)
+				data_list = []
+				for user in user_list:
+					watchrecords = WatchRecord.objects.filter(user=user).order_by('-create_time')
 
-			data_list = []
-			for user in user_list:
-				watchrecords = WatchRecord.objects.filter(user=user).order_by('-create_time')
-				print("wa", watchrecords)
-				if watchrecords.exists():
-					# 秒转时分秒
-					total_duration_seconds = watchrecords.annotate(sum=Sum("video_process")).values("sum")
-					total_duration_seconds = (list(total_duration_seconds))[0]["sum"]
-					m, s = divmod(total_duration_seconds, 60)
-					h, m = divmod(m, 60)
-					total_duration_seconds = ("%02d:%02d:%02d" % (h, m, s))
+					if watchrecords.exists():
+						# 秒转时分秒
+						total_duration_seconds = watchrecords.annotate(sum=Sum("video_process")).values("sum")
+						total_duration_seconds = (list(total_duration_seconds))[0]["sum"]
+						m, s = divmod(total_duration_seconds, 60)
+						h, m = divmod(m, 60)
+						total_duration_seconds = ("%02d:%02d:%02d" % (h, m, s))
 
-					watchrecord = watchrecords.first()
-					create_time = watchrecord.create_time.strftime("%Y-%m-%d %H:%M:%S")
-					progress = (watchrecord.video_process) / (watchrecord.duration) * 100
+						watchrecord = watchrecords.first()
+						create_time = watchrecord.create_time.strftime("%Y-%m-%d %H:%M:%S")
+						progress = (watchrecord.video_process) / (watchrecord.duration) * 100
 
-					result = {
-						"nickname": watchrecord.user.nickname,  # 姓名
-						"project": watchrecord.course.project.name,  # 项目
-						"course": watchrecord.course.name,  # 课程
-						"section": watchrecord.video.section.title,  # 章节
-						"video": watchrecord.video.name,  # 视频
-						"progress": "%d%%" % progress,  # 进度
-						"sum_progress": 11,  # 总进度
-						"total_duration_seconds": total_duration_seconds,  # 累计观看时间
-						"create_time": create_time,  # 最新登录时间
-						"xx": 0,  # 累计练习时间
-						"ss": 0,  # 累计考核时间
+						result = {
+							"nickname": watchrecord.user.nickname,  # 姓名
+							"project": watchrecord.course.project.name,  # 项目
+							"course": watchrecord.course.name,  # 课程
+							"section": watchrecord.video.section.title,  # 章节
+							"video": watchrecord.video.name,  # 视频
+							"progress": "%d%%" % progress,  # 进度
+							"sum_progress": 11,  # 总进度
+							"total_duration_seconds": total_duration_seconds,  # 累计观看时间
+							"create_time": create_time,  # 最新登录时间
+							"xx": 0,  # 累计练习时间
+							"ss": 0,  # 累计考核时间
 
-						"sex": watchrecord.user.get_sex_display(),  # 姓别
-						"institutions": watchrecord.user.institutions,  # 院校
-						"birthday": watchrecord.user.birthday,  # 生日
-						"is_graduate": watchrecord.user.is_graduate,  # 在校情况
-						"education": watchrecord.user.education,  # 学历
-						"is_computer": watchrecord.user.is_computer  # 是否计算机专业
-					}
+							"sex": watchrecord.user.get_sex_display(),  # 姓别
+							"institutions": watchrecord.user.institutions,  # 院校
+							"birthday": watchrecord.user.birthday,  # 生日
+							"is_graduate": watchrecord.user.is_graduate,  # 在校情况
+							"education": watchrecord.user.education,  # 学历
+							"is_computer": watchrecord.user.is_computer  # 是否计算机专业
+						}
 
-				else:
-					result = {
-						"nickname": user.nickname,  # 姓名
-						"sex": user.get_sex_display(),  # 姓别
-						"institutions": user.institutions,  # 院校
-						"birthday": user.birthday,  # 生日
-						"is_graduate": user.is_graduate,  # 在校情况
-						"education": user.education,  # 学历
-						"is_computer": user.is_computer  # 是否计算机专业
-					}
-				data_list.append(result)
+					else:
+						result = {
+							"nickname": user.nickname,  # 姓名
+							"sex": user.get_sex_display(),  # 姓别
+							"institutions": user.institutions,  # 院校
+							"birthday": user.birthday,  # 生日
+							"is_graduate": user.is_graduate,  # 在校情况
+							"education": user.education,  # 学历
+							"is_computer": user.is_computer  # 是否计算机专业
+						}
+					data_list.append(result)
+				data = data_list
+		except:
+			traceback.print_exc()
+			logging.getLogger().error(traceback.format_exc())
+			err = status.HTTP_500_INTERNAL_SERVER_ERROR
+			msg = traceback.format_exc()
 
-			data = data_list
+		finally:
+			return JsonResponse(data=data, err=err, msg=msg)
+
+
+class DateRangeInfo(APIView):
+	"""
+	日期区间信息
+	"""
+
+	def get(self, request, *args, **kwargs):
+		err = status.HTTP_204_NO_CONTENT
+		msg = "success"
+		data = list()
+
+		try:
+			name=self.request.GET.get("name","")
+			date=self.request.GET.get("date","")
+			start_date="2018-08-07"
+			end_date="2018-08-08"
+			if name:
+				param = dict(nickname__icontains=name)
+				param = get_kwargs(param)
+				user_list = CustomUser.objects.filter(**param)
+				data_list = []
+				for user in user_list:
+					print("user",user)
+					watchrecords = WatchRecord.objects.filter(user=user, create_time__range=[start_date,end_date])
+					print("wa",watchrecords)
+
+
+
+
 		except:
 			traceback.print_exc()
 			logging.getLogger().error(traceback.format_exc())
@@ -164,43 +202,49 @@ class StudentInfo(APIView):
 
 
 
-class TimeInfo(APIView):
-	"""
-	时间段信息
-	"""
+
+
+
+
 
 
 class LearningTime(APIView):
 	"""
 	时间分布
 	"""
-	def get(self,request,*args,**kwargs):
+
+	def get(self, request, *args, **kwargs):
 		err = status.HTTP_204_NO_CONTENT
 		msg = "success"
 		data = list()
 		try:
 			name = self.request.GET.get("name", "")  # 姓名
-
-			time = "2018-08-08" #self.request.GET.get("time", "")  # 时间
-
+			date = "2018-08-09"  # self.request.GET.get("date", "")  # 日期
 			param = dict(nickname__icontains=name)
 			param = get_kwargs(param)
 			user_list = CustomUser.objects.filter(**param)
-			print("user_list",user_list)
+
 			data_list = []
 			for user in user_list:
-
-				watchrecords = WatchRecord.objects.filter(user=user)
+				watchrecords = WatchRecord.objects.filter(user=user, create_time__startswith=date)
 				if watchrecords.exists():
-					watchrecord = watchrecords.first()
-					# time=watchrecord.create_time.strftime("%Y-%m-%d")
-					# section=watchrecord.video.section.title
+					sum_video_process = 0
+					for watchrecord in watchrecords:
+						video_process = watchrecord.video_process
+						sum_video_process += video_process  # 多个视频
 
+					m, s = divmod(sum_video_process, 60)
+					h, m = divmod(m, 60)
+					video_process = ("%02d:%02d:%02d" % (h, m, s))  # 视频时间
 
-					print("wa",watchrecord)
-
-
-
+					result = {
+						"video_process": video_process,  # 视频时间
+						"xtime": 0,  # 考核时间
+						"ytime": 0,  # 练习时间
+						"sumtime": 0,  # 总时间
+					}
+					data_list.append(result)
+			data = data_list
 
 		except:
 			traceback.print_exc()
@@ -210,7 +254,6 @@ class LearningTime(APIView):
 
 		finally:
 			return JsonResponse(data=data, err=err, msg=msg)
-
 
 
 class LearningVideo(APIView):
